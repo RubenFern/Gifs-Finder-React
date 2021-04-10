5d50cfb4460aed7d76280abda34f763158b8fca6
RubenFern <rubenferval@hotmail.com>
2021-04-10 16:59:39 +0200

    Comments added

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -2,9 +2,17 @@ import { useState } from "react";
 import { GifCard } from "./components/GifCard";
 import {SearchCategories} from "./components/SearchCategories";
 
+/**
+ * Componente principal de la aplicación.
+ * 
+ * Llama a la barra de búsqueda y a los GIFs.
+ * 
+ * Mediante un useState, SearchCategories almacena la búsqueda y guarda su valor.
+ * Ese valor es mandando a GifCard para realizar la búsqueda en la API
+ */
+
 export const GifApp = () =>
 {
-    // Prueba añadiendo elementos a la lista
     const [categories, setCategories] = useState([]);
 
     return (
