07210f605575bf43adf0611d526fa5d6ef9bec6f
RubenFern <rubenferval@hotmail.com>
2021-04-10 17:02:41 +0200

    Comments added. inputValue errors are handled

--- src/components/SearchCategories.jsx
+++ src/components/SearchCategories.jsx
@@ -3,25 +3,28 @@ import PropTypes from 'prop-types';
 
 export const SearchCategories = (props) =>
 {
-    const [inputValue, setInputValue] = useState();
+    const [inputValue, setInputValue] = useState('');
 
-    // I avoid sending the form
+    // I avoid sending the form. Se activa al enviar el formulario
     const handleSubmit = (e) =>
     {
         e.preventDefault();
 
-        if (inputValue.trim().length > 0)
+        // Si tiene contenido añado la búsqueda al final del array de las categorias
+        if (inputValue.length > 0)
         {
-            props.setCategories(gif => [...gif, inputValue]);
+            setInputValue(inputValue.trim());
+            props.setCategories(gif => [inputValue, ...gif]);
         }
     }
 
-    // Function to search GIFs
+    // Function to search GIFs. Cuando cambia el estado del input almaceno su valor
     const searchGif = (e) =>
     {
         setInputValue(e.target.value);
     }
 
+    // Muestra la barra de búsqueda
     return (
         <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Search GIFs" onChange={searchGif} name="" id=""/>
